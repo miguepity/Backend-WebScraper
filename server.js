ccad39cfa34c57c7deda3ec463839a5d58349fe7
miguepity <miguel.ardon@unitec.edu>
2018-09-14 09:44:23 -0600

    the final server for webscraper

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const request = require('request');
+const cheerio = require('cheerio');
+const app = express();
+const cors = require('cors');
+
+app.use(cors({
+    origin: '*'
+}))
+
+app.get('/scrape', function(req, res){
+    url = req.query.url;
+    var $;
+    request(url, function(error, response, html){
+        if(!error){
+            $ = cheerio.load(html);            
+            // console.log($.html('.row'));
+        }
+        res.send($.html());
+    });
+});
+
+app.listen('8081');
+console.log("on port 8081");
+exports = module.exports = app;
